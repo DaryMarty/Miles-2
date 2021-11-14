cbd7a56b76a3b8a519bcdddfd153ec742acdc31a
Darya Martemyanova <mdy.89@mail.ru>
2021-11-14 17:49:49 +0300

    Delete code

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,5 @@ public class Main {
         int price = 10_000;
         int miles = service.calculate(price);
         System.out.println(miles);
-
- ///      int ticketPrice = 5000;
-///        int bonusMileCost = 20;
-///        int numberOfBonusMiles = ticketPrice / bonusMileCost;
-///        System.out.println(numberOfBonusMiles);
     }
 }
